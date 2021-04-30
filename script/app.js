360e6eb85cde690d438d97c617c3e59ad8e839c9
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2021-04-30 11:33:39 -0400

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,1 +1,29 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+canvas.setAttribute("width", window.innerWidth + "px");
+canvas.setAttribute("height", window.innerHeight + "px");
 
+
+var caracter = {
+  x: 0, 
+  y: 0,
+  w: 75,
+  h:120,
+  image:document.getElementById('bob'),
+}
+
+function DrawCaracter () {
+ ctx.drawImage(caracter.image, caracter.x, caracter.y, caracter.w, caracter.h);
+}
+
+
+//----------------------------
+//GAME LOOP
+//----------------------------
+function loop () {
+  requestAnimationFrame(loop);
+  
+  
+   
+}
+requestAnimationFrame(loop);
