467ba64199ee2b7a6bf17e1810e1a71dadd3d11b
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2021-05-07 10:48:34 -0400

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,25 +1,48 @@
+//CANVAS SETUP
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.setAttribute("width", window.innerWidth + "px");
 canvas.setAttribute("height", window.innerHeight + "px");
 
 
+//CARACTER SETUP
+
 var caracter = {
-  x: 0, 
-  y: 0,
-  w: 75,
+  x:0, 
+  y:0,
+  w:75,
   h:120,
   image:document.getElementById('bob'),
+  draw:function(){
+    ctx.drawImage(caracter.image, caracter.x, caracter.y, caracter.w, caracter.h);
+  },
 }
 
-function DrawCaracter () {
- ctx.drawImage(caracter.image, caracter.x, caracter.y, caracter.w, caracter.h);
+
+//CONTROLS AND MOVEMENT
+var movement = {
+  left:false, 
+  right:false,
+  up:false,
+  down:false,
+  move:function(){
+    if(movement.left===true){carcater.x-=5;}
+    if(movement.right===true){carcater.x+=5;}
+    if(movement.up===true){carcater.y-=5;}
+    if(movement.down===true){carcater.y+=5;}
+  },
+}
+document.body.onkeydown = function(event){
+  var k = event.key;
+  if(k==="ArrowLeft" || k==="a"){movement.left=true;}
+  if(k==="ArrowRight" || k==="d"){movement.right=true;}
+  if(k==="ArrowUp" || k==="w"){movement.up=true;}
+  if(k==="ArrowDown" || k==="s"){movement.down=true;}
 }
 
-
-//----------------------------
 //GAME LOOP
-//----------------------------
+
 function loop () {
   requestAnimationFrame(loop);
   
